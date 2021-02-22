f8baeb38cccc8c08795f8de79681b307ec2956c4
Dingovina <65973711+Dingovina@users.noreply.github.com>
2021-02-22 20:10:22 +0500

    list_prof.html + task3 "Список профессий"

--- YL_WEB_flask-wtf/1_GettingReady.py
+++ YL_WEB_flask-wtf/1_GettingReady.py
@@ -16,5 +16,13 @@ def training(prof):
     return render_template('training.html', **params)
 
 
+@app.route('/list_prof/<type>')
+def list_prof(type):
+    list = ["инженер-исследователь", "пилот", "врач", "строитель", "эколог", "экзобиолог", "кламатолог",
+            "инженер по терраформированию", "геолог", "астрогеолог", "метеоролог", "специалист по радиационной защите"]
+    params = {'list': list, 'type': type, 'title': 'training'}
+    return render_template('list_prof.html', **params)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
