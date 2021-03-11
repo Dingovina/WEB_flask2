7bb3068b6e6eed1a3a8c03dbf8337fad17fb6e61
Dingovina <65973711+Dingovina@users.noreply.github.com>
2021-03-11 18:48:32 +0500

    rest api task 4

--- REST-API/data/jobs_api.py
+++ REST-API/data/jobs_api.py
@@ -2,7 +2,21 @@ import flask
 
 from . import db_session
 from .jobs import Jobs
-from flask import jsonify
+from flask import jsonify, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField, IntegerField
+from wtforms.validators import DataRequired
+
+
+class AddingJobForm(FlaskForm):
+    id = IntegerField('Job Id', validators=[DataRequired()])
+    job_title = StringField('Job title', validators=[DataRequired()])
+    team_leader_id = IntegerField('Team Leader id', validators=[DataRequired()])
+    work_size = IntegerField('Work size', validators=[DataRequired()])
+    collaborators = StringField('collaborators', validators=[DataRequired()])
+    is_finished = BooleanField('Is job finished?')
+    submit = SubmitField('Submit')
+
 
 blueprint = flask.Blueprint(
     'news_api',
@@ -11,19 +25,25 @@ blueprint = flask.Blueprint(
 )
 
 
-@blueprint.route('/api/jobs/')
-@blueprint.route('/api/jobs')
+@blueprint.route('/api/jobs/', methods=['GET', 'POST'])
+@blueprint.route('/api/jobs', methods=['GET', 'POST'])
 def get_jobs():
-    db_sess = db_session.create_session()
-    jobs = db_sess.query(Jobs).all()
-    return jsonify(
-        {
-            'jobs':
-                [item.to_dict(
-                    only=('id', 'team_leader', 'job_title', 'work_size', 'collaborators', 'is_finished', 'user_id'))
-                    for item in jobs]
-        }
-    )
+    form = AddingJobForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        job = Jobs()
+        job.id = form.id.data
+        job.job_title = form.job_title.data
+        job.team_leader = form.team_leader_id.data
+        job.work_size = form.work_size.data
+        job.collaborators = form.collaborators.data
+        job.is_finished = form.is_finished.data
+        db_sess.add(job)
+        db_sess.commit()
+        return 'Форма отправлена'
+    else:
+        render_template('add_job.html', title='New Job', form=form, message='Поля заполнены некорректно')
+    return render_template('add_job.html', title='New Job', form=form)
 
 
 @blueprint.route('/api/jobs/<job_id>')
