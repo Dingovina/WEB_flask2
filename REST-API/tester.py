4d4b446fd0e2224efaa15423df625c1f93c30c5f
Dingovina <65973711+Dingovina@users.noreply.github.com>
2021-03-18 18:01:29 +0500

    testing deleting

--- REST-API/tester.py
+++ REST-API/tester.py
@@ -2,14 +2,14 @@ import requests
 
 
 def response(arg):
-    ans = requests.get(f'http://127.0.0.1:5000/api/jobs/{arg}')
-    return ans.json()
+    ans = requests.get(f'http://127.0.0.1:5000/api/jobs/delete/{arg}')
+    return ans.text
 
 
-test1 = response(1)         # Корректный запрос
-test2 = response(2)         # Корректный запрос
-test3 = response('all')     # Корректный запрос на получение всех работ
-test4 = response(123)       # Некорректный запрос: неверный id
-test5 = response(0)         # Некорректный запрос: неверный id
-test6 = response('any')     # Некорректный запрос: строковый аргумент
-print(test1, test2, test3, test4, test5, test6, sep='\n')
+before = requests.get(f'http://127.0.0.1:5000/api/jobs/all').text
+test1 = response(1)  # Корректный запрос
+test4 = response(123)  # Некорректный запрос: неверный id
+test5 = response(0)  # Некорректный запрос: неверный id
+test6 = response('any')  # Некорректный запрос: строковый аргумент
+after = requests.get(f'http://127.0.0.1:5000/api/jobs/all').text
+print(before, test1, test4, test5, test6, after, sep='\n')
